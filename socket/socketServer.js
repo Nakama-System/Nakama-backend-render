afb23d1aafce2c7e0204b39c7449cd406770adc7
Nakama-System <system.nakama@gmail.com>
2026-03-17 00:13:31 -0300

    Update socketServer.js

--- socket/socketServer.js
+++ socket/socketServer.js
@@ -13,7 +13,7 @@ const Contact = require("../models/Contact");
 const Chat = require("../models/Chat");
 const { Block } = require("../models/BlockReport");
 const { uploadToCloudinary } = require("../services/cloudinaryService");
-const registerCommunitySocket = require("./SocketCommunity");
+const registerCommunitySocket = require("./Socketcommunity");
 const registerBattleSocket = require("./socketBattle");
 
 const JWT_SECRET = process.env.JWT_SECRET || "nakama_jwt_dev";
